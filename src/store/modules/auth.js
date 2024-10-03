6db410f11307c354838cbff247a789df916f17c0
Juan Faricelli <juan.faricelli@outlook.com>
2024-10-03 04:12:24 +0200

    user type to store

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -5,8 +5,14 @@ export default {
   state() {
     return {
       isAuthenticated: false,
+      userAuthType: null,
+      userProfile: null,
       authError: false,
       token: null,
+      isAdministrator: false,
+      isStaff: false,
+      isDoctor: false,
+      isPatient: false,
     };
   },
   mutations: {
@@ -22,6 +28,16 @@ export default {
     CLEAR_TOKEN(state) {
       state.token = null;
     },
+    SET_AUTH_TYPE(state, userAuthType) {
+      state.userAuthType = userAuthType;
+      state.isAdministrator = userAuthType === "admin";
+      state.isStaff = userAuthType === "staff";
+      state.isDoctor = userAuthType === "doctor";
+      state.isPatient = userAuthType === "patient";
+    },
+    SET_USER_PROFILE(state, userProfile) {
+      state.userProfile = userProfile;
+    },
   },
   actions: {
     // context is a default param
@@ -35,6 +51,11 @@ export default {
         .login(logInInformation)
         .then((data) => {
           context.commit("SET_AUTH", data.authenticated || false);
+          context.commit("SET_AUTH_TYPE", data.user.user_type || null);
+          context.commit("SET_USER_PROFILE", data.user.username || null);
+          context.commit("SET_TOKEN", data.token || null);
+          localStorage.setItem("sessionToken", data.token || null);
+
           context.commit("AUTH_LOGIN_ERROR", [403, 404].includes(data.code));
           context.dispatch("app/setServerUnavailable", data.code === 500, {
             root: true,
@@ -42,14 +63,14 @@ export default {
           context.dispatch("app/setComponentLoading", false, {
             root: true,
           });
-
-          const token = data.token;
-          context.commit("SET_TOKEN", token);
-          localStorage.setItem("sessionToken", token);
         })
         .catch((error) => {
           console.error("Login at modules failed with error:", error);
           context.commit("SET_AUTH", false);
+          context.commit("SET_AUTH_TYPE", null);
+          context.commit("SET_USER_PROFILE", null);
+          context.commit("SET_TOKEN", null);
+          localStorage.setItem("sessionToken", null);
         });
     },
     logout(context) {
@@ -72,6 +93,9 @@ export default {
     },
     getAuthError(state) {
       return state.authError;
+    },
+    getUserAuthType(state) {
+      return state.userAuthType;
     },
   },
 };
