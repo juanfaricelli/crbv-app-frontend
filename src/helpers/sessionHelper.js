1edd52465ce13452eb2ccf99bebc140c770dd4cf
Juan Faricelli <juan.faricelli@outlook.com>
2024-10-15 04:43:02 +0200

    implementing session valid validation and redirect to login if not

---
+++ src/helpers/sessionHelper.js
@@ -0,0 +1,39 @@
+// src/helpers/apiHelper.js
+import store from "@/store";
+import router from "@/router";
+
+export async function sessionHelper(url, method, options = {}) {
+  const sessionId = localStorage.getItem("sessionId") || "";
+  const Authorization = `Bearer ${sessionId}`;
+
+  const defaultOptions = {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization,
+    },
+  };
+
+  const finalOptions = { ...defaultOptions, ...options };
+
+  try {
+    const response = await fetch(url, finalOptions);
+
+    if (response.status === 403) {
+      // Handle session invalidation
+      console.error("Session is invalid. Logging out...");
+      store.commit("auth/CLEAR_SESSION");
+      store.commit("app/IS_COMPONENT_LOADING_UPDATED", false);
+      router.push({ name: "login" });
+      return { error: "Session is invalid" };
+    }
+
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error("API request failed:", error);
+    return { error: error.message };
+  }
+}
--- src/store/endpoints/patients.js
+++ src/store/endpoints/patients.js
@@ -1,29 +1,15 @@
+import { sessionHelper } from "@/helpers/sessionHelper";
+
 const domainConfig =
   process.env.NODE_ENV === "production"
     ? "https://crbv-app-backend.onrender.com"
     : "";
 
-let sessionId = "";
-let Authorization = "";
-let getPayload = {};
-const setSessionToken = () => {
-  sessionId = localStorage.getItem("sessionId") || "";
-  Authorization = `Bearer ${sessionId}`;
-  getPayload = {
-    method: "GET",
-    headers: {
-      Authorization,
-    },
-  };
-};
-
 export default {
   async getPatientById(userId) {
     try {
       const route = `${domainConfig}/api/user/patient`;
-      const patientData = await fetch(`${route}/${userId}`, getPayload).then(
-        (response) => response.json()
-      );
+      const patientData = await sessionHelper(`${route}/${userId}`, "GET");
       return patientData.length > 0
         ? Object.assign({}, patientData[0].user_data, {
             medical_record: patientData[0].medical_record,
@@ -36,13 +22,9 @@ export default {
   },
   async getPatientNewForm() {
     try {
-      setSessionToken();
+      // setSessionToken();
       const route = `${domainConfig}/api/user/patient/new`;
-      const patientNewForm = await fetch(`${route}`, getPayload).then(
-        (response) => {
-          return response.json();
-        }
-      );
+      const patientNewForm = await sessionHelper(route, "GET");
       return patientNewForm;
     } catch (error) {
       console.error(error);
@@ -51,21 +33,17 @@ export default {
   },
   async createNewPatient(newPatientData) {
     try {
-      setSessionToken();
+      // setSessionToken();
       const route = `${domainConfig}/api/user/patient/create`;
-      const patientNew = await fetch(route, {
+      const patientNew = await sessionHelper(route, "POST", {
         method: "POST", // *GET, POST, PUT, DELETE, etc.
         mode: "cors", // no-cors, *cors, same-origin
         cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
         credentials: "same-origin", // include, *same-origin, omit
-        headers: {
-          "Content-Type": "application/json",
-          Authorization,
-        },
         redirect: "follow", // manual, *follow, error
         referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
         body: JSON.stringify(newPatientData), // body data type must match "Content-Type" header
-      }).then((response) => response.json());
+      });
       return Object.assign({}, patientNew.user_data, {
         medical_record: patientNew.medical_record,
       });
@@ -76,21 +54,17 @@ export default {
   },
   async updatePatient(updatedPatientData) {
     try {
-      setSessionToken();
+      // setSessionToken();
       const route = `${domainConfig}/api/user/patient/${updatedPatientData.id_number}/update`;
-      const patientUpdated = await fetch(route, {
+      const patientUpdated = await sessionHelper(route, "PUT", {
         method: "PUT", // *GET, POST, PUT, DELETE, etc.
         mode: "cors", // no-cors, *cors, same-origin
         cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
         credentials: "same-origin", // include, *same-origin, omit
-        headers: {
-          "Content-Type": "application/json",
-          Authorization,
-        },
         redirect: "follow", // manual, *follow, error
         referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
         body: JSON.stringify(updatedPatientData), // body data type must match "Content-Type" header
-      }).then((response) => response.json());
+      });
       return patientUpdated
         ? Object.assign({}, patientUpdated.user_data, {
             medical_record: patientUpdated.medical_record,
